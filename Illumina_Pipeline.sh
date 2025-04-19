23c643162dba40624b2079d2bd2aea4c87aae024
floriantilliet <ninjadargent@gmail.com>
2025-04-19 18:29:49 +0200

    added readme

--- Illumina_Pipeline.sh
+++ Illumina_Pipeline.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 #
+DIR=$(pwd)
+OUTPUT=OUTPUT
 
-while getopts "i:f:r:" option
+while getopts "hi:o:f:r:m:d:c:" option
 do
         case $option in
+                h)
+                    echo "help"
+                    exit 0
+                    ;;
                 i)
                     INPUT="$OPTARG"
+                    ;;
+                o)
+                    OUTPUT="$OPTARG"
                     ;;
                 f)
                     PRIMER_F="$OPTARG"
@@ -14,13 +23,25 @@ do
                 r)
                     PRIMER_R="$OPTARG"
                     ;;
+                m)
+                    MAPPING="$OPTARG"
+                    ;;
+                d)
+                    DATABASE="$OPTARG"
+                    ;;
+                c)
+                    IDENTITY_PERCENTAGE="$OPTARG"
+                    ;;
+
         esac
 done
 
-./00_prepare_DB.sh
+mkdir $OUTPUT
 
-./01_run_cutadapt.sh
+./$0/bash_scripts/00_prepare_DB.sh -i $DATABASE -f $PRIMER_F -r $PRIMER_R
 
-./02_run_dada2.sh
+./$0/bash_scripts/01_merging_demultiplexing.sh -i $INPUT -o $OUTPUT -m $MAPPING
 
-Rscript -e "rmarkdown::render('$DIR/../report.rmd', params = list(dir='$DIR', output='$OUTPUT', depth='$DEPTH'), output_dir = '$DIR/', output_file = 'report_$OUTPUT.html')"
+./$0/bash_scripts/02_OTU_clustering.sh -c $IDENTITY_PERCENTAGE -o $OUTPUT
+
+Rscript -e "rmarkdown::render('$0/R_scripts/report.rmd', params = list(dir='$DIR', output='$OUTPUT', depth='$DEPTH'), output_dir = '$DIR/', output_file = 'report_$OUTPUT.html')"
